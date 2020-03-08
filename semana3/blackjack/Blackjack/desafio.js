f45fe39da03456d769cb59a9c0209b141d8e296e
Erika-Skarda <erikaskarda@yahoo.com.br>
2020-03-08 18:59:24 -0300

    Desafio do projeto da semana - 3

--- semana3/blackjack/Blackjack/desafio.js
+++ semana3/blackjack/Blackjack/desafio.js
@@ -27,33 +27,80 @@ if(confirm("Quer iniciar uma nova rodada?")) {
       const segundaCarta = comprarCarta();
 
    } else {
-      console.log("O usuário - cartas:  " + primeiraCarta.texto + " "+ segundaCarta.texto + " A carta revelada do computador é "+ primeiraPC.texto + "\n")
-         if(confirm("Deseja comprar mais uma carta?")) {
-            let soma = (primeiraCarta.valor + segundaCarta.valor)
-            for(let i = 0; i > 21; i++) {
-               let tentativas = comprarCarta()
-               soma = tentativas.valor + soma
-               console.log("Suas cartas são: " + (primeiraCarta.valor + segundaCarta.valor + tentativas.valor))
-            }
-            
-           // console.log("Suas cartas são: " + primeiraCarta.texto + " "+ segundaCarta.texto + " A carta revelada do computador é "+ primeiraPC.texto + "\n" + "Sua pontuação é: "  +  primeiraCarta.valor + segundaCarta.valor + tentativas.valor)
-         } else { 
-            
-            if((primeiraCarta.valor + segundaCarta.valor) === (primeiraPC.valor + segundaPC.valor) ) {
-               console.log("Empate")
-      
-               } else if ((primeiraCarta.valor + segundaCarta.valor) > (primeiraPC.valor + segundaPC.valor) ) {
-                  console.log("Usuário venceu!")
-                  
+
+      alert("O usuário - cartas:  " + primeiraCarta.texto + " e "+ segundaCarta.texto + "\n" + "A carta revelada do computador é "+ primeiraPC.texto)
+         
+      let soma = primeiraCarta.valor + segundaCarta.valor
+      let arrayCarta = [primeiraCarta.texto, segundaCarta.texto]
+      let arrayCartaValor = [primeiraCarta.valor, segundaCarta.valor]
+   
+         if(confirm("Deseja comprar mais uma carta?"))  {
+
+            while(soma <= 21) { 
+
+            let proxCarta = comprarCarta()
+            soma = proxCarta.valor + soma
+            arrayCartaValor.push(proxCarta.valor)
+            arrayCarta.push(proxCarta.texto)
+
+            alert("Suas cartas são: " + arrayCarta + ". A carta revelada do computador é " + 
+            primeiraPC.texto + "\n")
+            console.log("Suas cartas são: " + arrayCarta + ". As cartas do computador são: " + primeiraPC.texto + " e " + segundaPC.texto) 
+
+           } 
+               if(soma === (primeiraPC.valor + segundaPC.valor)) {
+                  alert("Suas cartas são: " + arrayCarta + ". Sua pontuação é: " + soma + "\n" + "As cartas do computador são: " + (primeiraPC.texto + " e " + segundaPC.texto)
+                  + " . A pontuação do computador é: " + (primeiraPC.valor + segundaPC.valor) +"\n"+ " Empate!!!")            
+               
+               } else if(soma > (primeiraPC.valor + segundaPC.valor) && soma <= 21) {
+                  alert("Suas cartas são: " + arrayCarta + ". Sua pontuação é: " + soma + "\n" + "As cartas do computador são: " + (primeiraPC.texto + " e " + segundaPC.texto)
+                  + " . A pontuação do computador é: " + (primeiraPC.valor + segundaPC.valor) + "\n" +" Usuário venceu!!!")  
+
                } else {
-                  console.log("Computador venceu!")
+                  alert("Suas cartas são: " + arrayCarta + ". Sua pontuação é: " + soma + "\n" + "As cartas do computador são: " + (primeiraPC.texto + " e " + segundaPC.texto)
+                  + " . A pontuação do computador é: " + (primeiraPC.valor + segundaPC.valor) + "\n" + " Computador venceu!!!")  
                }
+
+            
+            alert("FIM DE JOGO!!!")
+
+         } else { 
+             
+            let somaPC = primeiraPC.valor + segundaPC.valor
+            let arrayPCValor = [primeiraPC.valor, segundaPC.valor]
+            let arrayPC = [primeiraPC.texto, segundaPC.texto]
+            
+            while(somaPC <= 21) {
+
+               let proxCartaPC = comprarCarta()
+               somaPC = proxCartaPC.valor + somaPC
+               arrayPCValor.push(proxCartaPC.valor)
+               arrayPC.push(proxCartaPC.texto)
+
+            }
+                  if(soma === somaPC) {
+                  alert("Suas cartas são: " + arrayCarta + ". Sua pontuação é: " + soma
+                   + "\n" + "As cartas do computador são: " + arrayPC + " . A pontuação do computador é: " + somaPC + "\n" + " Empate!!!") 
+         
+               } else if((soma < somaPC) && somaPC <= 21){
+                  alert("Suas cartas são: " + arrayCarta + ". Sua pontuação é: " + soma
+                  + "\n" + "As cartas do computador são: " + arrayPC + " . A pontuação do computador é: " + somaPC +"\n"+ " O computador venceu!!! ") 
+                     
+               } else {
+                  alert("Suas cartas são: " + arrayCarta + ". Sua pontuação é: " + soma
+                   + "\n" + "As cartas do computador são: " + arrayPC + " . A pontuação do computador é: " + somaPC +"\n"+ " O usuário venceu!!!") 
+   
+               }
+   
+                  alert("FIM DE JOGO!!!")
+            
          } 
+      
    }
+
 } else {
 
       console.log("Jogo acabou")
    
    }
-
 
