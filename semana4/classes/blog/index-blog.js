b40691bd947c128f677b1af4dc8acec0a135150a
Erika-Skarda <erikaskarda@yahoo.com.br>
2020-03-11 15:01:24 -0300

    Exercício - TAREFA2

--- semana4/classes/blog/index-blog.js
+++ semana4/classes/blog/index-blog.js
@@ -1,59 +1,25 @@
-/*const arrayPares = []
-let qtdPares
+//EXERCÌCIO 2
 
-let seEPar = (num) => {
+class post {
 
-    for(i = 0; i <= num; i ++) {
-
-        if(i % 2 === 0) {
-            arrayPares.push(i)
-        }
-    }
-    
-    return console.log(arrayPares)
-}
-
-seEPar(10)
-
-let pares = (numero) => {
-    
-    let num = 0
-    for(i = 0; i <= numero; i ++) {
-
-        if(i % 2 === 0) {
-            num =+ i
-            console.log(num) 
-        }  
-    }    
-}
-pares(10) */
-
-/*
-const novoTexto = document.getElementById("container")
-novoTexto.innetHTML += "<p>Primeiro</p>"
-novoTexto.innetHTML += "<p>Segundo</p>"
-novoTexto.innetHTML += "<p>Terceiro</p>"
-novoTexto.innetHTML += "<p>PQuarto</p>"
-novoTexto.innetHTML += "<p>Quinto</p>"
-
-*/
-const novoTexto = document.getElementById("container")
-function adiciona(qtd) {
-    for(let i=0; i < qtd; i++) {
-        novoTexto.innerHTML += "<p>Primeiro</p>"
+    constructor(titulo, autor, conteudo) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.conteudo = conteudo;
     }
 }
-adiciona(1)
 
-let cliquesNoBotao = 0
-function alterarQuantidadeDeCliques () {
-    cliquesNoBotao += 1
-    const paragrafo = document.getElementsByTagName("p")[0]
-    paragrafo.innerHTML = "Quantidade de Cliques: " + cliquesNoBotao
+function onClickButton () {
+
+    let tituloNovo = document.getElementById("inputTitulo").value;
+    let autorNovo = document.getElementById("inputAutor").value;
+    let conteudoNovo = document.getElementById("inputConteudo").value;
+
+    let novoPost = new post(tituloNovo, autorNovo, conteudoNovo);
+
+    console.log(novoPost)
+        
+    tituloNovo.value = "";
+    autorNovo.value = "";
+    conteudoNovo.value ="";
 }
-
-
-
-
-
-
