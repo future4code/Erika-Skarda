8a9a201129d382c1b73e5455f2c225323fd10004
Erika-Skarda <erikaskarda@yahoo.com.br>
2020-03-11 21:33:29 -0300

    Exercício TAREFA 5 e Desafio 1

--- semana4/classes/blog/index-blog.js
+++ semana4/classes/blog/index-blog.js
@@ -1,11 +1,12 @@
-//EXERCÌCIO 3
+//EXERCÌCIO 5
 
 class post {
 
-    constructor(titulo, autor, conteudo) {
+    constructor(titulo, autor, conteudo, imagem) {
         this.titulo = titulo;
         this.autor = autor;
         this.conteudo = conteudo;
+        this.imagem = imagem;
     }
 }
 
@@ -14,10 +15,29 @@ function onClickButton () {
     let tituloNovo = document.getElementById("inputTitulo").value;
     let autorNovo = document.getElementById("inputAutor").value;
     let conteudoNovo = document.getElementById("inputConteudo").value;
+    let imagemNova = document.querySelector("#inputImagem").value
 
-    let novoPost = new post(tituloNovo, autorNovo, conteudoNovo);
-    console.log(novoPost);
+    if ((imagemNova.includes("http://") ||
+        imagemNova.includes("https://")) && (imagemNova.includes(".jpg") ||
+        imagemNova.includes(".png") || imagemNova.includes(".bmp"))) {
+            
+            let novoPost = new post(tituloNovo, autorNovo, conteudoNovo, imagemNova);
+            console.log(novoPost);
+        
+            let arrayPost = [tituloNovo, autorNovo, conteudoNovo, imagemNova];
+            console.log(arrayPost);
+            
+           // window.location.assign("index-post.html");
+            
+            const inserirTarefa = document.querySelector("#postSalvo")
+        
+                inserirTarefa.innerHTML += "<p>" + tituloNovo + "</p>" + "\n" +
+                "<p>" + autorNovo + "</p>" + "\n" + "<p>" + conteudoNovo + "\n" +
+                "<img src='" + imagemNova + "'>"
 
-    let arrayPost = [tituloNovo, autorNovo, conteudoNovo];
-    console.log(arrayPost);
+    } else {
+
+                alert("Não é um link de imagem válido")
+            }
+           
 }
